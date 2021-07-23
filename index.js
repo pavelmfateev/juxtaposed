174f1671e62ec735b98c1e4571fb2234289dc23b
Pasha <pavel.m.fateev@gmail.com>
2021-07-23 09:30:10 -0700

    add ejs-mate layouts

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express"),
   app = express(),
   path = require("path"),
-  port = 3000;
+  port = 3000,
+  ejsMate = require("ejs-mate");
 
 app.use(express.static(path.join(__dirname, "public")));
 // Views folder and EJS setup:
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
